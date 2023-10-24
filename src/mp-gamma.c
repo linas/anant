ebda869e3e1b9fb112d0ce2e9840010d788dbda3
Linas Vepstas <linasvepstas@gmail.com>
2023-10-23 23:16:10 -0500

    whitespace cleanup

--- src/mp-gamma.c
+++ src/mp-gamma.c
@@ -1,21 +1,20 @@
-
 /*
  * mp-gamma.c
  *
  * Compute gamma function for various complex arguments
  *
  * Copyright (C) 2006 Linas Vepstas
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
@@ -40,10 +39,10 @@
  * fp_lngamma -- compute log of gamma for real argument
  *
  * Uses simple, quickly converging algo-- A&S 6.1.33
- * Slightly modified: 
- * ln Gamma (z+2)=z(1-gamma)+sum_{n=2}^\infty (zeta(n)-1) z^n/n 
+ * Slightly modified:
+ * ln Gamma (z+2)=z(1-gamma)+sum_{n=2}^\infty (zeta(n)-1) z^n/n
  * Valid input must have 0 < x < 4
- * and ideally 1.5 < x < 2.5 for fastest convergence 
+ * and ideally 1.5 < x < 2.5 for fastest convergence
  */
 static void reduced_lngamma (mpf_t gam, const mpf_t ex, int prec)
 {
@@ -58,7 +57,7 @@ static void reduced_lngamma (mpf_t gam, const mpf_t ex, int prec)
 	mpf_set (z, ex);
 
 	mpf_set_ui (gam, 0);
-	
+
 	mpf_sub_ui (z, z, 2);
 	mpf_mul (zn, z,z);
 
@@ -66,7 +65,7 @@ static void reduced_lngamma (mpf_t gam, const mpf_t ex, int prec)
 	mpf_t maxterm;
 	mpf_init (maxterm);
 	fp_epsilon (maxterm, prec);
-	
+
 	n=2;
 	while (1)
 	{
@@ -108,10 +107,10 @@ static void reduced_lngamma (mpf_t gam, const mpf_t ex, int prec)
  *
  * Same code as above, extended for complex arguments
  * Uses simple, quickly converging algo-- A&S 6.1.33
- * Slightly modified: 
- * ln Gamma (z+2)=z(1-gamma)+sum_{n=2}^\infty (zeta(n)-1) z^n/n 
+ * Slightly modified:
+ * ln Gamma (z+2)=z(1-gamma)+sum_{n=2}^\infty (zeta(n)-1) z^n/n
  * Valid input must have |ex-2| < 2
- * and ideally |ex-2| < 0.5 for fastest convergence 
+ * and ideally |ex-2| < 0.5 for fastest convergence
  */
 static void cpx_reduced_lngamma (cpx_t gam, const cpx_t ex, int prec)
 {
@@ -126,7 +125,7 @@ static void cpx_reduced_lngamma (cpx_t gam, const cpx_t ex, int prec)
 	cpx_set (z, ex);
 
 	cpx_set_ui (gam, 0, 0);
-	
+
 	cpx_sub_ui (z, z, 2, 0);
 	cpx_mul (zn, z, z);
 
@@ -134,7 +133,7 @@ static void cpx_reduced_lngamma (cpx_t gam, const cpx_t ex, int prec)
 	mpf_t maxterm;
 	mpf_init (maxterm);
 	fp_epsilon (maxterm, 2*prec);
-	
+
 	n=2;
 	while (1)
 	{
@@ -143,7 +142,7 @@ static void cpx_reduced_lngamma (cpx_t gam, const cpx_t ex, int prec)
 		mpf_sub_ui (term[0].re, term[0].re, 1);
 		mpf_div_ui (term[0].re, term[0].re, n);
 		cpx_mul (term, term, zn);
-		
+
 		if (n%2)
 		{
 			cpx_sub (gam, gam, term);
@@ -174,10 +173,10 @@ static void cpx_reduced_lngamma (cpx_t gam, const cpx_t ex, int prec)
 }
 
 /* ================================================= */
-/* 
- * gamma function, valid only for 0 < x < 4 
+/*
+ * gamma function, valid only for 0 < x < 4
  * Otherwise explodes...
- */ 
+ */
 static inline void reduced_gamma (mpf_t gam, const mpf_t ex, int prec)
 {
 	reduced_lngamma (gam, ex, prec);
@@ -185,7 +184,7 @@ static inline void reduced_gamma (mpf_t gam, const mpf_t ex, int prec)
 }
 
 /* ================================================= */
-/* 
+/*
  * fp_gamma
  * Use pochhammer to get into range of 1.5 < z < 2.5,
  * Then use the reduced summation formula. The range
@@ -198,15 +197,15 @@ void fp_gamma (mpf_t gam, const mpf_t z, int prec)
 
 	/* make a copy of the input arg NOW! */
 	mpf_set (zee, z);
-	
-	/* double-presision used, this code doesn't need to 
-	 * be all that accurate; just need a reasonable int part. 
+
+	/* double-presision used, this code doesn't need to
+	 * be all that accurate; just need a reasonable int part.
 	 */
 	double flo = mpf_get_d (zee);
 	if (flo > 2.5)
 	{
 		unsigned int intpart = (unsigned int) floor (flo-1.0);
-		
+
 		/* The goal of the next if statement is to make sure that
 		 * -0.5<zee<0.5, which helps maintain excellent convergence.
 		 */
@@ -235,7 +234,7 @@ void fp_gamma (mpf_t gam, const mpf_t z, int prec)
 	mpf_t rgamma;
 	mpf_init (rgamma);
 	reduced_gamma (rgamma, zee, prec);
-	
+
 	mpf_mul (gam, gam, rgamma);
 
 	mpf_clear (zee);
@@ -274,13 +273,13 @@ void mpf_gamma_cache (mpf_t gam, const mpf_t z, int prec)
 }
 
 /* ================================================= */
-/* 
+/*
  * gamma function, but valid only for -2 < ImZ < 2 (and any Re z).
  *
  * Use pochhammer to get into range of 1.5 < Re Z < 2.5,
  * Then use the reduced summation formula. The range
  * of 1.5 < ReZ < 2.5 is particularly quickly convergent
- */ 
+ */
 
 static void cpx_reduced_gamma (cpx_t gam, const cpx_t z, int prec)
 {
@@ -289,15 +288,15 @@ static void cpx_reduced_gamma (cpx_t gam, const cpx_t z, int prec)
 
 	/* Make a copy of the input arg NOW! */
 	cpx_set (zee, z);
-	
+
 	/* double-presision used; its just for the if-tests, and so doesn't
-	 * need to be all that accurate; just need a reasonable int part. 
+	 * need to be all that accurate; just need a reasonable int part.
 	 */
 	double flo = mpf_get_d (zee[0].re);
 	if (flo > 2.5)
 	{
 		unsigned int intpart = (unsigned int) floor (flo-1.0);
-		
+
 		/* The goal of the next if statement is to make sure that
 		 * -0.5<zee<0.5, which helps maintain excellent convergence.
 		 */
@@ -341,11 +340,11 @@ static void cpx_reduced_gamma (cpx_t gam, const cpx_t z, int prec)
 }
 
 /* ================================================= */
-/* 
+/*
  * gamma function for general complex argument
  *
  * Use the multiplication theorem to compute result.
- */ 
+ */
 void cpx_gamma (cpx_t gam, const cpx_t z, int prec)
 {
 	/* Step one: find out how big the imaginary part is */
@@ -357,7 +356,7 @@ void cpx_gamma (cpx_t gam, const cpx_t z, int prec)
 		cpx_reduced_gamma (gam, z, prec);
 		return;
 	}
-	
+
 	/* Step two: prepare to use the multiplicatin theorem */
 	cpx_t zee, mzee, term, acc;
 	cpx_init (zee);
@@ -367,7 +366,7 @@ void cpx_gamma (cpx_t gam, const cpx_t z, int prec)
 
 	/* Copy the input arg NOW! */
 	cpx_set (mzee, z);
-	
+
 	mpf_t frac;
 	mpf_init (frac);
 
@@ -376,7 +375,7 @@ void cpx_gamma (cpx_t gam, const cpx_t z, int prec)
 	/* frac = 1/m */
 	mpf_set_ui (frac, 1);
 	mpf_div_ui (frac, frac, m);
-	
+
 	cpx_set_ui (acc, 1, 0);
 
 	/* Apply the multiplication theorem */
@@ -408,10 +407,10 @@ void cpx_gamma (cpx_t gam, const cpx_t z, int prec)
 
 	cpx_set (gam, acc);
 
-	cpx_clear (zee); 
-	cpx_clear (mzee); 
-	cpx_clear (term); 
-	cpx_clear (acc); 
+	cpx_clear (zee);
+	cpx_clear (mzee);
+	cpx_clear (term);
+	cpx_clear (acc);
 	mpf_clear (frac);
 }
 
@@ -421,14 +420,14 @@ void cpx_gamma_cache (cpx_t gam, const cpx_t z, int prec)
 	static int precision = 0;
 	int redo = 0;
 
-	if (!precision)
-	{
-		cpx_init (cache_z);
-		cpx_init (cache_gam);
-	}
-	
 	if (precision < prec)
 	{
+		if (!precision)
+		{
+			cpx_init (cache_z);
+			cpx_init (cache_gam);
+		}
+
 		cpx_set_prec (cache_z, 3.322*prec+50);
 		cpx_set_prec (cache_gam, 3.322*prec+50);
 		precision = prec;
